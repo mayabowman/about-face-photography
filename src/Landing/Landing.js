d3585ef8745fadbeb349f2bf56919c1ecd6abff0
Maya Bowman <mayashady1@gmail.com>
2020-02-17 19:23:52 -0500

    added images

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Landing.scss';
 
 class Landing extends React.Component {
   render() {
+    let names = [
+      'AmberB', 'AmberR', 'Gilbert', 'Graffy', 'Kraig', 'Lenny', 'Lisette1', 'Lisette2',
+      'Mac', 'Maria', 'Maya1', 'Maya2', 'Mechaela', 'Michelle', 'Steve'
+    ]
+    const photoDisplay = names.map((name, i) => {
+      return <img key={i} src={require(`../images/${name}.jpg`)} className='headshot'alt='headshot'/>
+    })
+
+    console.log('photoDisplay', photoDisplay[0].props.src)
     return (
-      <p>Photos will go here</p>
+      <div>
+        <p>Photos will go here</p>
+        {photoDisplay}
+      </div>
     )
   }
 }
--- src/photo-store.js
+++ src/photo-store.js
@@ -1,9 +1,95 @@
 export default {
   "photos": [
-    {
-      id: 1,
-      name: "AmberB",
-      src: "./images/AmberB.jpg"
-    }
+    "../images/AmberB.jpg",
+    "../images/AmberR.jpg",
+    "../images/Gilbert.jpg",
+    "../images/Graffy.jpg",
+    "../images/Kraig.jpg",
+    "../images/Lenny.jpg",
+    "../images/Lisette1.jpg",
+    "../images/Lisette2.jpg",
+    "../images/Mac.jpg",
+    "../images/Maria.jpg",
+    "../images/Maya1.jpg",
+    "../images/Maya2.jpg",
+    "../images/Mechaela.jpg",
+    "../images/Michelle.jpg",
+    "../images/Steve.jpg",
   ]
 }
+
+// {
+//   id: 1,
+//   name: "AmberB",
+//   src: "./images/AmberB.jpg"
+// },
+// {
+//   id: 2,
+//   name: "AmberR",
+//   src: "./images/AmberR.jpg"
+// },
+// {
+//   id: 3,
+//   name: "Gilbert",
+//   src: "./images/Gilbert.jpg"
+// },
+// {
+//   id: 4,
+//   name: "Graffy",
+//   src: "./images/Graffy.jpg"
+// },
+// {
+//   id: 5,
+//   name: "Kraig",
+//   src: "./images/Kraig.jpg"
+// },
+// {
+//   id: 6,
+//   name: "Lenny",
+//   src: "./images/Lenny.jpg"
+// },
+// {
+//   id: 7,
+//   name: "Lisette1",
+//   src: "./images/Lisette1.jpg"
+// },
+// {
+//   id: 8,
+//   name: "Lisette2",
+//   src: "./images/Lisette2.jpg"
+// },
+// {
+//   id: 9,
+//   name: "Mac",
+//   src: "./images/Mac.jpg"
+// },
+// {
+//   id: 10,
+//   name: "Maria",
+//   src: "./images/Maria.jpg"
+// },
+// {
+//   id: 11,
+//   name: "Maya1",
+//   src: "./images/Maya1.jpg"
+// },
+// {
+//   id: 12,
+//   name: "Maya2",
+//   src: "./images/Maya2.jpg"
+// },
+// {
+//   id: 13,
+//   name: "Mechaela",
+//   src: "./images/Mechaela.jpg"
+// },
+// {
+//   id: 14,
+//   name: "Michelle",
+//   src: "./images/Michelle.jpg"
+// },
+// {
+//   id: 15,
+//   name: "Steve",
+//   src: "./images/Steve.jpg"
+// },
