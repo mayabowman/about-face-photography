2f93ff26ab13571c7b4b557f09bb3b4666a26f30
Maya Bowman <mayashady1@gmail.com>
2020-02-18 15:33:57 -0500

    worked on images

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -5,17 +5,18 @@ class Landing extends React.Component {
   render() {
     let names = [
       'AmberB', 'AmberR', 'Gilbert', 'Graffy', 'Kraig', 'Lenny', 'Lisette1', 'Lisette2',
-      'Mac', 'Maria', 'Maya1', 'Maya2', 'Mechaela', 'Michelle', 'Steve'
+      'Mac', 'Maria', 'Maya1', 'Maya2', 'Michelle', 'Steve'
     ]
     const photoDisplay = names.map((name, i) => {
       return <img key={i} src={require(`../images/${name}.jpg`)} className='headshot'alt='headshot'/>
     })
 
-    console.log('photoDisplay', photoDisplay[0].props.src)
     return (
-      <div>
-        <p>Photos will go here</p>
+      <div className='photo-div'>
         {photoDisplay}
+        <div className='sq-div'>
+          <img src={require('../images/Mechaela.jpg')} id='square-img' alt='headshot'/>
+        </div>
       </div>
     )
   }
