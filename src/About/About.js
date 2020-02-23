2eb47ff3f392e369c4ad709da97669ecf5e4b197
Maya Bowman <mayashady1@gmail.com>
2020-02-23 08:25:09 -0500

    added about content

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './About.scss';
 
 function About() {
   return (
+    <div className='about'>
     <h2>What I Do</h2>
+    <p>Thanks for taking a minute to read this. I’m a Midwest photographer focusing on
+      headshots and portraits for the individual trying to make their way, be acknowledged,
+      and be seen for what they can bring to a situation, a community, a life.
+      This means listening, vision, and some good lighting go a long way towards proving potential,
+      of which a portrait is a capsule. What a photo session offers is an exchange of perspectives
+      on character. And if a camera can capture that exchange, it is truly a gift.</p>
+    </div>
   )
 }
 
