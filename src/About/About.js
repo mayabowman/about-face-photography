bff4197bd839d7bedab97f2fd9cdd841045bf34b
Maya Bowman <mayashady1@gmail.com>
2020-03-09 12:48:46 -0400

    added image to about page

--- src/About/About.js
+++ src/About/About.js
@@ -3,15 +3,18 @@ import './About.scss';
 
 function About() {
   return (
-    <div className='about'>
+    <div className='about-page'>
     <h2>What I Do</h2>
-    <p>Thanks for taking a minute to read this. I’m a photographer focusing on headshots
-      and portraits for the individual trying to make their way and be acknowledged.
-      For the person needing to be seen for what they can bring to a situation, a community, a life.
-      This means listening, vision, and some good lighting go a long way towards proving potential.
-      A portrait is a capsule of the potential of character.  What a photo session offers is an
-      exchange of perspectives on character. And if a camera can capture that exchange,
-      it is truly a gift.</p>
+    <div className='about'>
+      <p>Thanks for taking a minute to read this. I’m a photographer focusing on headshots
+        and portraits for the individual trying to make their way and be acknowledged.
+        For the person needing to be seen for what they can bring to a situation, a community, a life.
+        This means listening, vision, and some good lighting go a long way towards proving potential.
+        A portrait is a capsule of the potential of character.  What a photo session offers is an
+        exchange of perspectives on character. And if a camera can capture that exchange,
+        it is truly a gift.</p>
+        <img src={require('../images/about-page-pic.jpeg')} alt='camera in hands' className='about-pic'/>
+      </div>
     </div>
   )
 }
